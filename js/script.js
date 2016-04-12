166393d0a9b7ab21f5dc802a2d206169b86092f8
bcpiotr <piotr.nowak@post.com>
2016-04-12 17:51:47 +0200

    Portfolio preview and progress bars scripted

--- js/script.js
+++ js/script.js
@@ -39,6 +39,8 @@ WORK PREVIEW - LINKS
 var basicAnimationTime = 1500;
 var links = $('#work').find('li');
 console.log(links);
+
+
 links.each(function(index){
   $(this).on('click',function(){
     var j = $(this).data('id'); //gets id from data set
@@ -55,8 +57,8 @@ links.each(function(index){
 /**************************************
 WORK PREVIEW - IMG HIDE
 **************************************/
+var images = $('#preview').find('img'); //array of images
 function imgHide(){
-  var images = $('#preview').find('img');
   images.each(function(){
     $(this).fadeOut(basicAnimationTime);
   });
@@ -68,6 +70,78 @@ function imgShow(j){
   var activeImg = $('#preview').find('img:eq('+j+')');
   console.log(activeImg);
   activeImg.fadeIn(basicAnimationTime);
+  checkButtons(j);
 }
+/**************************************
+WORK PREVIEW - NEXT/PREV HIDE/SHOW
+**************************************/
+function checkButtons(j){
+  console.log('test');
+  if (j === images.length-1) {
+    $('#preview').find('.arrowNext').css('display','none'); //hides next arrow at the end of gallery
+  }
+  else if (j === 0) {
+    $('#preview').find('.arrowPrev').css('display','none');
+  }
+  else {
+    $('#preview').find('.arrowNext').css('display','block'); //shows next arrow
+    $('#preview').find('.arrowPrev').css('display','block');
+  }
+};
 
+/**************************************
+WORK PREVIEW - NEXT BUTTON
+**************************************/
+var nextBtn = $('#preview').find('.arrowNext');
+console.log(nextBtn);
+
+nextBtn.on('click',function(){
+  var id = $('#work').find('.activeLi'); //checks which link is active
+  j = id.data('id');
+  id.toggleClass('activeLi').next().toggleClass('activeLi');
+  imgHide();
+  setTimeout(function(){
+    imgShow(id.data('id')+1);
+  },basicAnimationTime);
+});
+
+/**************************************
+WORK PREVIEW - PREV BUTTON
+**************************************/
+var prevBtn = $('#preview').find('.arrowPrev');
+
+prevBtn.on('click',function(){
+  var id = $('#work').find('.activeLi'); //checks which link is active
+  j = id.data('id');
+  id.toggleClass('activeLi').prev().toggleClass('activeLi');
+  imgHide();
+  setTimeout(function(){
+    imgShow(id.data('id')-1);
+  },basicAnimationTime);
+});
+
+/**************************************
+WORK PREVIEW - ONMOUSE EVENT
+**************************************/
+$('#preview').on('mouseover', function(){
+  $('div[class*="arrow"]').css('opacity','1');
+});
+$('#preview').on('mouseout', function(){
+  $('div[class*="arrow"]').css('opacity','0');
+});
+
+/**************************************
+PROGRESS BARY
+**************************************/
+$(window).on('scroll',function(){
+  $('.skillbar').each(function(){
+    $(this).find('.skillbar-bar').animate({
+      width:$(this).attr('data-percent')
+    },6000);
+  });
+});
+
+
+
+//
 });
