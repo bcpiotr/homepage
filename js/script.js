521a2c9dfc6500435b7c761f939831a712512ed5
bcpiotr <piotr.nowak@post.com>
2016-04-12 16:39:10 +0200

    portfolio preview done

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ MENU
 *****************************/
 var menuButton = $('#menu').find('.img');
 
-console.log(menuButton);
-
 menuButton.on('mouseover',function(){
   $(this).find('img').attr("src","img/elipsa_blue.png");
 });
@@ -34,5 +32,42 @@ aboutMeElipsis.on('click',function(){
   aboutMeElipsis.toggleClass("fa-caret-square-o-down").toggleClass("fa-caret-square-o-up");
 })
 
+
+/**************************************
+WORK PREVIEW - LINKS
+**************************************/
+var basicAnimationTime = 1500;
+var links = $('#work').find('li');
+console.log(links);
+links.each(function(index){
+  $(this).on('click',function(){
+    var j = $(this).data('id'); //gets id from data set
+    links.removeClass('activeLi');
+    $(this).addClass('activeLi');
+    imgHide();
+    setTimeout(function(){
+      imgShow(j);
+    },basicAnimationTime);
+
+  })
+});
+
+/**************************************
+WORK PREVIEW - IMG HIDE
+**************************************/
+function imgHide(){
+  var images = $('#preview').find('img');
+  images.each(function(){
+    $(this).fadeOut(basicAnimationTime);
+  });
+}
+/**************************************
+WORK PREVIEW - IMG SHOW
+**************************************/
+function imgShow(j){
+  var activeImg = $('#preview').find('img:eq('+j+')');
+  console.log(activeImg);
+  activeImg.fadeIn(basicAnimationTime);
+}
 
 });
